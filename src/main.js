b893f2ef6f81749d279b40ca4b3e8119686f7ae4
Muhammad Wafa <wafax.4@gmail.com>
2021-03-30 03:00:01 +0700

    without await

--- src/main.js
+++ src/main.js
@@ -10,54 +10,51 @@ try {
   const password = core.getInput("password").trim()
   const config = core.getInput("config").trim()
 
-  await new Promise((resolve, rejects) => {
-    core.warning("Donwloading config file")
-    https
-      .get(config, (res) => {
-        if (res.statusCode === 200) {
-          const file = fs.createWriteStream("__config.ovpn")
-          res.pipe(file)
-          file.on("finish", () => {
-            file.close()
-            resolve()
+  core.warning("Donwloading config file")
+  https
+    .get(config, (res) => {
+      if (res.statusCode === 200) {
+        const file = fs.createWriteStream("__config.ovpn")
+        res.pipe(file)
+        file.on("finish", () => {
+          file.close()
+
+          fs.writeFileSync("__up.txt", [username, password].join("\n"))
+          fs.writeFileSync("openvpn.log", "")
+
+          const tail = new Tail("openvpn.log")
+
+          try {
+            shell.exec(
+              `sudo openvpn --config __config.ovpn --daemon --log openvpn.log --auth-user-pass __up.txt`
+            )
+          } catch (error) {
+            core.error(fs.readFileSync("openvpn.log", "utf8"))
+            tail.unwatch()
+            throw error
+          }
+
+          tail.on("line", (data) => {
+            core.info(data)
+            if (data.includes("Initialization Sequence Completed")) {
+              core.info("VPN connected successfully.")
+              tail.unwatch()
+              clearTimeout(timer)
+            }
           })
-        } else {
-          rejects("Download error")
-        }
-      })
-      .on("error", (error) => {
-        rejects(error.message)
-      })
-  })
 
-  fs.writeFileSync("__up.txt", [username, password].join("\n"))
-  fs.writeFileSync("openvpn.log", "")
-
-  const tail = new Tail("openvpn.log")
-
-  try {
-    shell.exec(
-      `sudo openvpn --config __config.ovpn --daemon --log openvpn.log --auth-user-pass __up.txt`
-    )
-  } catch (error) {
-    core.error(fs.readFileSync("openvpn.log", "utf8"))
-    tail.unwatch()
-    throw error
-  }
-
-  tail.on("line", (data) => {
-    core.info(data)
-    if (data.includes("Initialization Sequence Completed")) {
-      core.info("VPN connected successfully.")
-      tail.unwatch()
-      clearTimeout(timer)
-    }
-  })
-
-  const timer = setTimeout(() => {
-    core.setFailed("VPN connection failed.")
-    tail.unwatch()
-  }, 15000)
+          const timer = setTimeout(() => {
+            core.setFailed("VPN connection failed.")
+            tail.unwatch()
+          }, 15000)
+        })
+      } else {
+        throw new Error("Download error!")
+      }
+    })
+    .on("error", (error) => {
+      throw error
+    })
 } catch (error) {
   core.setFailed(error.message)
 }
