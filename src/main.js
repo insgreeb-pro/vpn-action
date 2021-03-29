d752f7a6d3aa0a78404f1d86ed6fdf3cf6b20043
Muhammad Wafa <wafax.4@gmail.com>
2021-03-30 04:00:54 +0700

    simple action

--- src/main.js
+++ src/main.js
@@ -1,6 +1,46 @@
 const exec = require("./exec")
+const fs = require("fs")
+const Tail = require("tail")
 
 module.exports = () => {
   // installing dependencies
-  exec("sudo apt install openvpn")
+  const config = core.getInput("config").trim()
+  const username = core.getInput("username").trim()
+  const password = core.getInput("password").trim()
+
+  const cfg = "__config.ovpn"
+  const auth = "__up.txt"
+  const log = "openvpn.log"
+
+  exec("sudo apt install openvpn openvpn-systemd-resolved")
+  exec(`curl ${config}>${cfg}`)
+
+  fs.writeFileSync(auth, [username, password].join("\n"))
+  fs.writeFileSync(log, "")
+
+  const tail = new Tail(log)
+
+  try {
+    exec(
+      `sudo openvpn --config ${cfg} --daemon --auth-user-pass ${auth} --log ${log}`
+    )
+  } catch (error) {
+    core.error(fs.readFileSync("openvpn.log", "utf8"))
+    tail.unwatch()
+    throw error
+  }
+
+  tail.on("line", (data) => {
+    core.info(data)
+    if (data.includes("Initialization Sequence Completed")) {
+      core.info("VPN connected successfully.")
+      tail.unwatch()
+      clearTimeout(timer)
+    }
+  })
+
+  const timer = setTimeout(() => {
+    core.setFailed("VPN connection failed.")
+    tail.unwatch()
+  }, 15000)
 }
